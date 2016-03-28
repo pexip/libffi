d2ac6a120a32516483450bc02de592063d1bc509
Nirbheek Chauhan <nirbheek@centricular.com>
2016-03-28 20:34:48 +0530

    Port to the Meson build system
    Also fixes some inconsistency between usage of #ifdef and #if inside C macros
    Include support for universal builds and multilib builds via suffixed
    arch-specific headers in ${prefix}/include + various other fixes.

--- include/ffi_common.h
+++ include/ffi_common.h
@@ -48,12 +48,10 @@ char *alloca ();
 #endif
 
 /* Check for the existence of memcpy. */
-#if STDC_HEADERS
+#if HAVE_MEMCPY
 # include <string.h>
 #else
-# ifndef HAVE_MEMCPY
-#  define memcpy(d, s, n) bcopy ((s), (d), (n))
-# endif
+# define memcpy(d, s, n) bcopy ((s), (d), (n))
 #endif
 
 #if defined(FFI_DEBUG)
