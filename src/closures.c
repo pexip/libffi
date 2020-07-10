b1088cd3eb3b116a23ad5735c97e39f34bd43fb5
John-Mark Bell <jmb@pexip.com>
2020-07-10 14:39:10 +0100

    Closures: /dev/shm or bust

--- src/closures.c
+++ src/closures.c
@@ -728,15 +728,6 @@ static struct
   { open_temp_exec_file_memfd, "libffi", 0 },
 #endif
   { open_temp_exec_file_dir, "/dev/shm", 0 },
-  { open_temp_exec_file_env, "LIBFFI_TMPDIR", 0 },
-  { open_temp_exec_file_env, "TMPDIR", 0 },
-  { open_temp_exec_file_dir, "/tmp", 0 },
-  { open_temp_exec_file_dir, "/var/tmp", 0 },
-  { open_temp_exec_file_env, "HOME", 0 },
-#ifdef HAVE_MNTENT
-  { open_temp_exec_file_mnt, "/etc/mtab", 1 },
-  { open_temp_exec_file_mnt, "/proc/mounts", 1 },
-#endif /* HAVE_MNTENT */
 };
 
 /* Current index into open_temp_exec_file_opts.  */
